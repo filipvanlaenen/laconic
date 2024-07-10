2496dc8cca3f029c4ec09c149599f78cab5281e1
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2024-07-10 11:59:14 +0200

    Issue #2: Initial implementation of the call to log an error

--- src/main/java/net/filipvanlaenen/laconic/Laconic.java
+++ src/main/java/net/filipvanlaenen/laconic/Laconic.java
@@ -1,5 +1,26 @@
 package net.filipvanlaenen.laconic;
 
-public class Laconic {
+import java.io.PrintStream;
 
+/**
+ * Base class for the logging operations.
+ */
+public class Laconic {
+    /**
+     * The default object to log to.
+     */
+    public static final Laconic LOGGER = new Laconic();
+    /**
+     * A PrintStream to which the log messages can be appended.
+     */
+    private PrintStream appender = System.err;
+
+    /**
+     * Logs an error to the appender.
+     *
+     * @param message The message describing the error.
+     */
+    public void logError(final String message) {
+        appender.println(message);
+    }
 }
